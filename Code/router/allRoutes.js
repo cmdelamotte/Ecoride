b88aa8d78c3134b0406519e7d345accd518dd4bf
Charles <charlesmax.delamotte@gmail.com>
2025-05-09 00:00:33 +0200

    Ajout espace admin

--- Code/router/allRoutes.js
+++ Code/router/allRoutes.js
@@ -9,6 +9,7 @@ export const allRoutes = [
     new Route("/edit-password", "Changement de mot de passe","/pages/auth/edit-password.html",["client", "admin"]),
     new Route("/employee-login", "Connexion employé", "/pages/auth/employee-login.html",["disconnected"]),
     new Route("/employee-dashboard", "Espace employé", "/pages/auth/employee-dashboard.html",["disconnected"]),
+    new Route("/admin-dashboard", "Espace employé", "/pages/auth/admin-dashboard.html",["disconnected"]),
     new Route("/forgot-password", "Mot de passe oublié", "/pages/auth/forgot-password.html",["disconnected"]),
     new Route("/your-rides", "Vos covoiturages", "/pages/booking/your-rides.html", ["client"]),
     new Route("/book-ride", "Réserver", "/pages/booking/book-ride.html", ["client"]),
